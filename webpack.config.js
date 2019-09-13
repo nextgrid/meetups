0d908c2a523625ec63fc6a2bed2d2eba62d5b325
Mateusz Bajorek <m.bajorek2@student.uw.edu.pl>
2019-09-13 10:41:44 +0200

    Front-end pre-alpha

--- webpack.config.js
+++ webpack.config.js
@@ -32,8 +32,8 @@ module.exports = {
         ]
       },
       {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        test: /\.s?css$/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
