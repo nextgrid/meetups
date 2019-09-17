3e0b08deaa167d4bbcad8c60ef0c833e108c5e39
elMarco <elmarcopl@gmail.com>
2019-09-17 11:02:15 +0200

    Fixed server default port.

--- src/server/server.js
+++ src/server/server.js
@@ -60,5 +60,7 @@ app.get('*', (req, res) => {
   });
 });
 
-const PORT = 3000;
-app.listen(PORT);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
